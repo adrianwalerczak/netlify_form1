0e923b582e65e8b3f1e142a9937d9b487375df81
Adrian ALX <adrianwalerczak@gmail.com>
2023-11-06 13:32:20 +0100

    console.log cookies

--- script.js
+++ script.js
@@ -19,5 +19,5 @@ document.cookie = "nazwa_ciasteczka=wartosc_ciasteczka; expires=Thu, 01 Jan 2099
 
 document.cookie = "my_cookie=my_sensitive_data; HttpOnly; expires=Thu, 01 Jan 2099 00:00:00 UTC; path=/";
 
-
+console.log(document.cookie)
 

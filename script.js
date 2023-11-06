d806747da42a4e38e96afc93e9cfc7dadab8c290
Adrian ALX <adrianwalerczak@gmail.com>
2023-11-06 10:37:00 +0100

    testowe

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("loginForm");
+
+    loginForm.onsubmit = function () {
+        console.log("abc");
+        return false; // Zapobiegamy domyślnej akcji przesyłania formularza
+    };
+});

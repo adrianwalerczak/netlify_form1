fdf27a95093268842ba0e64a2bd607966efadf46
Adrian ALX <adrianwalerczak@gmail.com>
2023-11-06 13:29:15 +0100

    dodanie ciasteczek

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ localStorage.setItem("language", "pl");
 
 sessionStorage.setItem("cosTam", "alaMaKota");
 
+// Ustawienie ciasteczka
+document.cookie = "nazwa_ciasteczka=wartosc_ciasteczka; expires=Thu, 01 Jan 2099 00:00:00 UTC; path=/";
+
 document.cookie = "my_cookie=my_sensitive_data; HttpOnly; expires=Thu, 01 Jan 2099 00:00:00 UTC; path=/";
 
 
